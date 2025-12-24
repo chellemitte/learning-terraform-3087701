b616b7fb07ed421653a0e4198b285a006c47d0f8
Michelle Taylor <michelletaylor@Michelles-MacBook-Pro.local>
2025-12-23 21:46:22 -0500

    Fix 1

--- main.tf
+++ main.tf
@@ -14,36 +14,47 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    environment = "dev"
   }
 }
 
-
-module "blog_autoscaling" {
+module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
+  version = "9.0.2"
 
-  name = "blog"
+  name = "blog-asg"
+  min_size = 1
+  max_size = 2
 
-  min_size            = 1
-  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  security_groups     = [module.blog_sg.security_group_id]
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id      = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
+
 }
+
+resource "aws_autoscaling_attachment" "asg_alb" {
+  autoscaling_group_name = "blog-asg"
+  lb_target_group_arn   = module.blog_alb.target_group_arns[0]
+}
+
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
@@ -60,33 +71,40 @@ module "blog_alb" {
   target_groups = [
     {
       name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
+      protocol         = "HTTP"
+      port             = 80
       target_type      = "instance"
     }
   ]
 
   http_tcp_listeners = [
     {
-      port               = 80
-      protocol           = "HTTP"
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
       target_group_index = 0
     }
   ]
 
   tags = {
-    Environment = "dev"
+    environment = "dev"
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "5.3.1"
+  name = "blog"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
-  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  vpc_id = module.blog_vpc.vpc_id
+
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
-  egress_rules = ["all-all"]
+
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
