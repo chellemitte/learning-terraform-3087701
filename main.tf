72b4ff088fdf8652c5e82fb1d69212633a5f4a7d
Michelle Taylor <michelletaylor@Michelles-MacBook-Pro.local>
2025-12-23 21:31:25 -0500

    Fix blog_alb again and again

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
+  alb_target_group_arns   = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
@@ -66,22 +66,26 @@ module "blog_alb" {
   target_groups = [
     {
       name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
+      protocol         = "HTTP"
+      port             = 80
       target_type      = "instance"
     }
   ]
 
-  http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
     }
-  ]
+  }
 
   tags = {
-    Environment = "dev"
+    environment = "dev"
   }
 }
 
